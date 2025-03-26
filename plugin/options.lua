935c859c72e2e68a4df950a278f10d7e33f97cfa
Elias Rojas <rojas.elias@outlook.com>
2025-03-25 22:21:53 -0300

    first commit

---
+++ plugin/options.lua
@@ -0,0 +1,21 @@
+local opt = vim.opt
+
+----- Interesting Options -----
+
+-- You have to turn this one on :)
+opt.inccommand = "split"
+
+-- Best search settings :)
+opt.smartcase = true
+opt.ignorecase = true
+
+----- Personal Preferences -----
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.signcolumn = "yes"
+opt.shada = { "'10", "<0", "s10", "h" }
+
+opt.clipboard = "unnamedplus"
+opt.guicursor = ""
